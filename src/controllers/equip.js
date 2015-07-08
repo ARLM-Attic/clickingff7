5984e54a5bc0319edc7273f77c2ce4d258a53c17
Menencia <menencia@gmail.com>
2015-07-09 00:18:09 +0200

    feat(equip): show the changed stats when weapon selected

--- src/controllers/equip.js
+++ src/controllers/equip.js
@@ -39,10 +39,76 @@ class EquipController extends Controller {
 
     /**
      *
-     * @param weapon
+     * @param newWeapon
      */
-    toggleWeapon(weapon) {
-        this.selected = (this.selected != weapon) ? weapon : null;
+    toggleSelect(newWeapon) {
+
+        if (this.selected != newWeapon) {
+            this.selected = newWeapon;
+            this.compareStats();
+        } else {
+            this.selected = null;
+            this.final = {};
+        }
+
+    }
+
+    /**
+     *
+     */
+    compareStats() {
+        // compare stats
+        this.final = {};
+        for (let i in this.selected.data.stats) {
+
+            // base character stat
+            let curr = this.character[i];
+
+            let final = curr;
+
+            // if the character has a weapon and the stat
+            let currWeapon = this.character.weapon;
+            if (currWeapon && currWeapon.data.stats[i]) {
+                final -= currWeapon.data.stats[i];
+            }
+
+            // add the selected stat
+            final += this.selected.data.stats[i];
+
+            // curr, chande, color
+            this.final[i] = {
+                curr  : curr,
+                change: final,
+                color : (final > curr) ? 'green' : 'red'
+            };
+        }
+    }
+
+    /**
+     *
+     * @param stat
+     * @returns {boolean}
+     */
+    isChanged(stat) {
+        if (this.final) {
+            let s = this.final[stat];
+            return s ? (s.curr != s.change) : false;
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param stat
+     * @param key
+     * @returns {*}
+     */
+    getFinal(stat, key = 'change') {
+        if (this.final) {
+            let s = this.final[stat];
+            return s ? s[key] : '';
+        }
+        return null;
     }
 
 }
